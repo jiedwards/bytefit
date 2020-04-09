386ac32fbfa53c2cf5cb45b30132e95467f3e421
Jacob Edwards <jacobedwards1995@hotmail.co.uk>
2020-04-09 15:39:54 +0100

    generated basic spring project with dependencies

---
+++ src/main/java/com/bytefit/MealPlanner/MealPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.bytefit.MealPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytefit/MealPlanner/MealPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytefit.MealPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
