b465a1a844656a39122bf001b6aca772941788e4
Jacob Edwards <jacobedwards1995@hotmail.co.uk>
2020-04-12 17:20:26 +0100

    styling for html and java controller to serve webpages

--- src/main/java/com/bytefit/MealPlanner/controller/ApplicationController.java
+++ src/main/java/com/bytefit/MealPlanner/controller/ApplicationController.java
@@ -1,4 +1,26 @@
 package com.bytefit.MealPlanner.controller;
 
+import com.bytefit.MealPlanner.entity.User;
+import com.bytefit.MealPlanner.repository.UserRepository;
+import com.bytefit.MealPlanner.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
 public class ApplicationController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService service;
+
+    @RequestMapping("/")
+    public String viewHomePage() {
+        return "index";
+    }
 }
