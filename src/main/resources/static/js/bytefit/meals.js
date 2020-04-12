3faa7b609627258a7d2b9b6caaac5dafc9138d05
Jacob Edwards <jacobedwards1995@hotmail.co.uk>
2020-04-12 17:42:42 +0100

    added functionality for complex meal plan call from api

--- src/main/resources/static/js/bytefit/meals.js
+++ src/main/resources/static/js/bytefit/meals.js
@@ -10,3 +10,15 @@ function generate_simple_mealplan() {
     .then(data => { return console.log(data.json()) })
     .then(res => { console.log(res) })
 }
+
+async function generate_complex_mealplan() {
+  const calories = document.getElementById("calories").value;
+  const num_meals = document.getElementById("num_meals").value;
+
+  const url = 'https://api.spoonacular.com/recipes/complexSearch?sort=random&maxCalories=' + calories + '&instructionsRequired=true&addRecipeNutrition=true&number=' + num_meals + '&apiKey=' + api_key;
+
+  var response = await fetch(url);
+  var data = await response.json();
+  console.log(data);
+
+}
