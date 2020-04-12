7b7b859de72e8ac06c760b964e661d166e0816ba
Jacob Edwards <jacobedwards1995@hotmail.co.uk>
2020-04-12 17:47:17 +0100

    added functionality to generate meal table on UI dependent on API response

--- src/main/resources/static/js/bytefit/meals.js
+++ src/main/resources/static/js/bytefit/meals.js
@@ -1,4 +1,12 @@
 import { api_key } from './credentials.js'
+const meal_parameters = {
+  'title': 'Recipe Name',
+  'dishTypes': 'Meal Type',
+  'analyzedInstructions': 'Instructions',
+  'image': '',
+  'nutrition': 'Nutritional Data',
+  'readyInMinutes': 'Time to prepare'
+};
 
 function generate_simple_mealplan() {
 
@@ -20,5 +28,27 @@ async function generate_complex_mealplan() {
   var response = await fetch(url);
   var data = await response.json();
   console.log(data);
+
+  let table_data = data.results;
+  let table = document.querySelector("table");
+  //Ensures that table is reset after second button click.
+  table.innerHTML = "";
+  let table_row_headers = Object.keys(table_data[0]);
+  generateTableHead(table, single_data_object);
+}
+
+
+function generateTableHead(table, data) {
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (let key of data) {
+    if (meal_parameters.hasOwnProperty(key)) {
+      let th = document.createElement("th");
+      let text = document.createTextNode(meal_parameters[key]);
+      console.log(meal_parameters[key])
+      th.appendChild(text);
+      row.appendChild(th);
+    }
+  }
 
 }
