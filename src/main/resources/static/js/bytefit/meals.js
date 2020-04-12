e8100ff6cd065423f2d7545308bc261f300b3c41
Jacob Edwards <jacobedwards1995@hotmail.co.uk>
2020-04-12 17:51:46 +0100

    added functionality to populate table with data

--- src/main/resources/static/js/bytefit/meals.js
+++ src/main/resources/static/js/bytefit/meals.js
@@ -7,6 +7,7 @@ const meal_parameters = {
   'nutrition': 'Nutritional Data',
   'readyInMinutes': 'Time to prepare'
 };
+const desired_nutrient_data = new Set(['Calories', 'Fat', 'Carbohydrates', 'Protein']);
 
 function generate_simple_mealplan() {
 
@@ -33,22 +34,38 @@ async function generate_complex_mealplan() {
   let table = document.querySelector("table");
   //Ensures that table is reset after second button click.
   table.innerHTML = "";
+  document.getElementById("recipeButton").innerHTML = "Re-generate";
   let table_row_headers = Object.keys(table_data[0]);
   generateTableHead(table, single_data_object);
+  generateTableData(table, table_data);
 }
 
 
 function generateTableHead(table, data) {
   let thead = table.createTHead();
   let row = thead.insertRow();
-  for (let key of data) {
-    if (meal_parameters.hasOwnProperty(key)) {
+  for (let property_header of data) {
+    if (meal_parameters.hasOwnProperty(property_header)) {
       let th = document.createElement("th");
-      let text = document.createTextNode(meal_parameters[key]);
-      console.log(meal_parameters[key])
+      let text = document.createTextNode(meal_parameters[property_header]);
       th.appendChild(text);
       row.appendChild(th);
     }
   }
 
 }
+
+function generateTableData(table, data) {
+  for (let element of data) {
+    let row = table.insertRow();
+    for (key in element) {
+      if (meal_parameters.hasOwnProperty(key)) {
+        let cell = row.insertCell();
+        let cell_text = document.createTextNode(element[key])
+        cell.appendChild(cell_text);
+        break;
+      }
+    }
+  }
+}
+
