4df1c57a4fa5fe88bc357719500f1969551f882c
Jacob Edwards <jacobedwards1995@hotmail.co.uk>
2020-04-12 17:34:38 +0100

    added ui with no functionality for login

--- src/main/java/com/bytefit/MealPlanner/controller/ApplicationController.java
+++ src/main/java/com/bytefit/MealPlanner/controller/ApplicationController.java
@@ -1,14 +1,11 @@
 package com.bytefit.MealPlanner.controller;
 
-import com.bytefit.MealPlanner.entity.User;
 import com.bytefit.MealPlanner.repository.UserRepository;
 import com.bytefit.MealPlanner.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 public class ApplicationController {
@@ -22,5 +19,10 @@ public class ApplicationController {
     @RequestMapping("/")
     public String viewHomePage() {
         return "index";
+    }
+
+    @RequestMapping("/login")
+    public String viewLoginPage(Model model) {
+        return "login";
     }
 }
