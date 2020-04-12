d873cd17e8932e7f8a5dc76ec8b50b7cd2739aab
Jacob Edwards <jacobedwards1995@hotmail.co.uk>
2020-04-12 17:35:44 +0100

    added register web page and java servlet

--- src/main/java/com/bytefit/MealPlanner/MealPlannerApplication.java
+++ src/main/java/com/bytefit/MealPlanner/MealPlannerApplication.java
@@ -1,9 +1,11 @@
 package com.bytefit.MealPlanner;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class MealPlannerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/bytefit/MealPlanner/controller/ApplicationController.java
+++ src/main/java/com/bytefit/MealPlanner/controller/ApplicationController.java
@@ -1,11 +1,17 @@
 package com.bytefit.MealPlanner.controller;
 
+import com.bytefit.MealPlanner.entity.User;
 import com.bytefit.MealPlanner.repository.UserRepository;
 import com.bytefit.MealPlanner.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class ApplicationController {
@@ -24,5 +30,10 @@ public class ApplicationController {
     @RequestMapping("/login")
     public String viewLoginPage(Model model) {
         return "login";
+    }
+
+    @RequestMapping("/register")
+    public String viewRegisterPage(Model model) {
+        return "register";
     }
 }
