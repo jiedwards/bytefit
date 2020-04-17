f87f44f7faeacada7f7bf51dac9142b87e41b41b
Jacob Edwards <jacobedwards1995@hotmail.co.uk>
2020-04-17 14:22:50 +0100

    added repository functionality and more tests for service

--- src/main/java/com/bytefit/MealPlanner/repository/RoleRepository.java
+++ src/main/java/com/bytefit/MealPlanner/repository/RoleRepository.java
@@ -1,4 +1,10 @@
 package com.bytefit.MealPlanner.repository;
 
-public class RoleRepository {
+import com.bytefit.MealPlanner.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByRole(String role);
 }
--- src/test/java/com/bytefit/MealPlanner/service/UserServiceTest.java
+++ src/test/java/com/bytefit/MealPlanner/service/UserServiceTest.java
@@ -1,7 +1,58 @@
 package com.bytefit.MealPlanner.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.bytefit.MealPlanner.entity.User;
+import com.bytefit.MealPlanner.repository.RoleRepository;
+import com.bytefit.MealPlanner.repository.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-class UserServiceTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class UserServiceTest {
+
+    private UserRepository userRepository = mock(UserRepository.class);
+    private RoleRepository roleRepository = mock(RoleRepository.class);
+    private BCryptPasswordEncoder bCryptPasswordEncoder = mock(BCryptPasswordEncoder.class);
+
+    private UserService userService;
+    private User user;
+    final String EMAIL = "test@test.com";
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+        userService = new UserService(bCryptPasswordEncoder,
+                userRepository,
+                roleRepository);
+        user = User.builder()
+                .id(1)
+                .firstname("Paul")
+                .lastname("Felder")
+                .email("test@test.com")
+                .build();
+
+        Mockito.when(userRepository.save(any()))
+                .thenReturn(user);
+        Mockito.when(userRepository.findByEmail(anyString()))
+                .thenReturn(user);
+    }
+
+    @Test
+    public void testFindUserByEmail() {
+        final User result = userService.findUserByEmail(EMAIL);
+        assertEquals(EMAIL, result.getEmail());
+    }
+
+    @Test
+    public void testSaveUser() {
+        User result = userService.saveUser(User.builder().build());
+        assertEquals(EMAIL, result.getEmail());
+    }
 
 }
