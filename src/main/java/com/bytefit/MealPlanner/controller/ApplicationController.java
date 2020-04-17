1aed4c38d2bdc63bae4edae54b9ac9d3d27f5707
Jacob Edwards <jacobedwards1995@hotmail.co.uk>
2020-04-17 15:29:02 +0100

    added backend functionality to handle HTTP requests

--- src/main/java/com/bytefit/MealPlanner/controller/ApplicationController.java
+++ src/main/java/com/bytefit/MealPlanner/controller/ApplicationController.java
@@ -4,14 +4,15 @@ import com.bytefit.MealPlanner.entity.User;
 import com.bytefit.MealPlanner.repository.UserRepository;
 import com.bytefit.MealPlanner.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
+import javax.validation.Valid;
 
 @Controller
 public class ApplicationController {
@@ -20,20 +21,60 @@ public class ApplicationController {
     private UserRepository userRepository;
 
     @Autowired
-    private UserService service;
+    private UserService userService;
 
     @RequestMapping("/")
     public String viewHomePage() {
         return "index";
     }
 
-    @RequestMapping("/login")
-    public String viewLoginPage(Model model) {
-        return "login";
+    @RequestMapping(value="/login", method = RequestMethod.GET)
+    public ModelAndView viewLoginPage(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
     }
 
-    @RequestMapping("/register")
-    public String viewRegisterPage(Model model) {
-        return "register";
+    @RequestMapping(value="/register", method = RequestMethod.GET)
+    public ModelAndView viewRegisterPage(){
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("register");
+        return modelAndView;
     }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        User userExists = userService.findUserByEmail(user.getEmail());
+        if (userExists != null) {
+            bindingResult
+                    .rejectValue("email", "error.user",
+                            "There is already a user registered with the email provided");
+        }
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("register");
+        } else {
+            userService.saveUser(user);
+            modelAndView.addObject("successMessage", "User has been registered successfully");
+            modelAndView.addObject("user", new User());
+            modelAndView.setViewName("register");
+
+        }
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/admin/dashboard", method = RequestMethod.GET)
+    public ModelAndView home(){
+        ModelAndView modelAndView = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
+        modelAndView.addObject("email", "Welcome " + user.getFirstname() + " " + user.getLastname() + " (" + user.getEmail() + ")");
+        modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
+        modelAndView.setViewName("admin/dashboard");
+        return modelAndView;
+    }
+
+
 }
